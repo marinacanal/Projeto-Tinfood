7b9ac1c1564025b9cb4b9cb4df2d6e142a4d992b
Marina Canal <121324624+marinacanal@users.noreply.github.com>
2023-10-31 21:47:53 -0300

    Delete registration-login-springboot-security-thymeleaf-master directory

--- registration-login-springboot-security-thymeleaf-master/src/main/java/com/example/registrationlogindemo/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.registrationlogindemo.repository;
-
-import com.example.registrationlogindemo.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
